50bdf4d0c0462703f055bc7a4d5cf476c0ebdb78
Austin Rivas <arivas@pushhere.com>
2017-12-27 19:45:41 -0500

    initial commit

---
+++ scripts/startup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker pull austinrivas/nginx-php-grav:1.0.0
+docker run -d -i -p 80:80 -p 2222:22 -v ~/repos/crowd-up/grav:/usr/share/nginx/html/ -name crowd-up-grav austinrivas/nginx-php-grav:1.0.0
